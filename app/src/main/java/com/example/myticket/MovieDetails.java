6dda4c8c8fc88b39f15dbed8cf0e7e75b483c0fe
basma11elmihy <basma.elmihy@gmail.com>
2019-06-02 17:11:54 +0200

    FAB added and toolbar- movie details

--- app/src/main/java/com/example/myticket/MovieDetails.java
+++ app/src/main/java/com/example/myticket/MovieDetails.java
@@ -1,13 +1,47 @@
 package com.example.myticket;
 
+import android.content.Intent;
+import android.support.annotation.Px;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MovieDetails extends AppCompatActivity {
+    private Button dropDown;
+    private com.robertlevonyan.views.customfloatingactionbutton.FloatingActionButton floatingActionButton;
+    private android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_movie_details);
+        dropDown = findViewById(R.id.dropdown_revs);
+        floatingActionButton = findViewById(R.id.custom_fab);
+//        toolbar = findViewById(R.id.toolbar);
+//        toolbar.setTitle("Movie Name");
+
+        dropDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  fabText.setVisibility(View.VISIBLE);
+                floatingActionButton.setFabText("");
+            }
+        });
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MovieDetails.this,HomeCinema.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
 }
